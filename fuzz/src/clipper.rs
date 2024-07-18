3924bc4114e6d3f118b27f58d8662430ba66a9e0
Dave Mollen <davemollen@gmail.com>
2024-07-18 14:32:48 +0200

    Replace simd with regular array operations

--- fuzz/src/clipper.rs
+++ fuzz/src/clipper.rs
@@ -1,25 +1,24 @@
-mod oversample;
-use oversample::Oversample;
-use std::simd::{f32x8, StdFloat};
+mod fir_filter;
+use {crate::shared::float_ext::FloatExt, fir_filter::FirFilter};
+
+const OVERSAMPLE_FACTOR: f32 = 8.;
 
 pub struct Clipper {
-  oversample: Oversample,
+  upsample_fir: FirFilter,
+  downsample_fir: FirFilter,
 }
 
 impl Clipper {
   pub fn new() -> Self {
     Self {
-      oversample: Oversample::new(),
+      upsample_fir: FirFilter::new(),
+      downsample_fir: FirFilter::new(),
     }
   }
 
   pub fn process(&mut self, input: f32) -> f32 {
-    self.oversample.process(input, |x| {
-      let x2 = x * x;
-      let x3 = x2 * x;
-      let x5 = x3 * x2;
-      let a = x + f32x8::splat(0.16489087) * x3 + f32x8::splat(0.00985468) * x5;
-      a / (f32x8::splat(1.0) + a * a).sqrt()
-    }) * 0.305496 // 0.610992 * 0.5
+    let upsampled = self.upsample_fir.process([input * OVERSAMPLE_FACTOR; 8]);
+    let clipped = upsampled.map(|x| x.fast_tanh1());
+    self.downsample_fir.process(clipped).into_iter().sum::<f32>() * 0.305496 // 0.610992 * 0.5
   }
 }
